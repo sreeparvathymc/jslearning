522c225cd4ee96f727ba1254c5d56e9ec1bd129b
sreeparvathy <sreeparvathymc61@gmail.com>
2024-07-05 13:31:09 +0530

    comit new

--- hof/index.js
+++ hof/index.js
@@ -1,42 +1,65 @@
-// map
+//************************1***********/ map**************************** 
+
 // const array = ["parvathy","gayathri","jagan","alinta"]
+
 // array.map((element,index)=>{
 //     console.log(element)
 // })
-// *******************************************************************
+
+// ************************2*****map**************************************
+
+
 // Prefer this method:-
+
 // const array = ["parvathy","gayathri","jagan","alinta"]
+
 // let names = array.map((element,index)=>{
 //     return element;
 // })
 // console.log(names)
-// *********************************************************************
-// forEach
+
+
+// ************************3******// forEach***************************************
+
 // let arr =["fruit","veg"]
+
 // let item = arr.forEach((elem)=> return elem)
 // console.log(item)
 
 // Cannot return like map
 
-// *********************************************************
+
+// ****************************4***4**push************************
+
 // let arr = [];
 // arr.push("Parvathy");
 // console.log(arr)
 
+// ******************************5***filter*************************
+
+
 // let arr =["a","b","c","d"]
+
 // let filteredVal = arr.filter((ele) => {
 //     return ele == "a"
 // })
 // console.log(filteredVal)
-// ******************************************************************
+
+
+// *********************************find the array sum using for loop*********************************
+
 // let arr = [15,16,17,18,19];
+
 // let sum = 0;
 // for(let i = 0; i<arr.length; i++ ){
 //     sum = sum + arr[i];
 // }
 // console.log(sum)
-// ******************************************************************
+
+// *********************the  the sum of odd index*********************************************
+
 // const arr = [1,2,3,4,5]
+
 // let sum = 0;
 // for(i = 0; i < arr.length; i++){
 //     if(i % 2 != 0 ){
@@ -44,22 +67,38 @@
 //     }  
 // }
 // console.log(sum)
+
 // ******************************************************************
+
 // const arr = [1,2,3,4,]
+
 // sum = 0;
 // for(let i = 0; i < arr.length; i++){
 //     sum = sum + arr[i]
 // }
 // console.log(sum)
+
+
 // ******************************reduce************************************
-// reduce
+
+
 // const arr = [1,2,3,4,]
+
 // let ans = arr.reduce((acc,elem,i) => {
 //     return acc+elem
 // },0)
 // console.log(ans)
-// *******************************************reduce********************
+
+// ******************************************
+// ******************************************
+// ******************************************
+// ******************************************
+// ******************************************
+// ******************************************
+// ******************************************
+// *************************************1******sum of odd index using reduce method*******************
 // const arr = [1,2,3,4,5,88,43,98];
+
 // let ans = arr.reduce((acc,elem,i) => {
 //     if (i % 2 != 0){
 //         acc = acc+elem
@@ -67,8 +106,10 @@
 //     return acc
 // },0)
 // console.log(ans)
-// **********************************************************************
-// e sum of elements of odd index
+
+
+// *********2******sum of elements of odd index using for loop*******************************************************
+ 
 // const arr = [1,2,3,4,5]
 // let sum = 0;
 // for(i = 0; i < arr.length; i++){
@@ -77,8 +118,10 @@
 //     }  
 // }
 // console.log(sum)
-// **************************************reduce********************************
-// In reduce sum of elements of even index
+
+// **************3***************In reduce sum of elements of even index*********reduce********************************
+
+// 
 // const arr = [1,2,3,4,5];
 // let ans = arr.reduce((acc,elem,i) => {
 //     if(i % 2 == 0){
@@ -87,7 +130,9 @@
 //     return acc
 // },0)
 // console.log(ans)
-// **********************************map********************************
+
+// **********************4**********numberofeven elements in array using map********************************
+
 // const arr = [1,2,3,4,5,6,7,8,9,10,11,12]
 // let even = 0;
 // let count = arr.map((element,index) => {
@@ -96,36 +141,40 @@
 //     }
 // })
 // console.log(even)
-// *******************************map*******************************
+
+
+// ********************5****in an array of object print hello + name*******map*******************************
+
+
 // const arr = [
 //     {Name:"John", Age:"50"},
 //     {Name:"Doe", Age:"25"},
 //     { Name:"celin", Age:"15"}
 //     ]
+
 // arr.map((element,index) =>{
 // console.log("Hello " + element.Name)
 // })
-// *************************sum using map****************************
+
+
+// ********************6*****sum using map****************************
+
+
 // const arr = [
 //     {name:"Tomato", price:50},
 //     {name:"Orange", price:25},
 //     { name:"Potato", price:15}
 //     ]
+
 // sum = 0;
 // const result = arr.map((element,index) => {
 //     sum = sum + element.price
 // }) 
 // console.log(sum)
-// **************************************************************
+
+// *******************7*****reduce**************************************
 
 
-
-// **************************************************************
-
-
-
-
-// ************************reduce**************************************
 // const arr = [
 //     {name:"Tomato", price:50},
 //     {name:"Orange", price:25},
@@ -136,7 +185,11 @@
 //   return acc
 // },0)
 // console.log(result);
-// ***************************************************
+
+
+// ***************************8***filter the obj arr of age less than 25*********************
+
+
 // var voters = [
 //     {name:'Bob' , age: 30, voted: true},
 //     {name:'Jake' , age: 32, voted: true},
@@ -156,7 +209,11 @@
 //         console.log(e.name)
 //     }
 // })
-// ********************************************************************************
+
+
+// ***********************9*****with map display the obj of age less than 25*(only names in a arr)***************************************************
+
+
 // var voters = [
 //     {name:'Bob' , age: 30, voted: true},
 //     {name:'Jake' , age: 32, voted: true},
@@ -177,7 +234,9 @@
 //     return ele.name
 // })
 // console.log(filtervoters)
-// ***********************************************************************************
+
+
+// *****************************10 in this age below 20 makes the voted elem false******************************************************
 
 // var voters = [
 //     {name:'Bob' , age: 30, voted: true},
@@ -193,6 +252,8 @@
 //     {name: 'Jeff', age: 30, voted: true},
 //     {name: 'Zack', age: 19, voted: true}
 // ];
+
+
 // let result = voters.map((ele) => {
 //     if(ele.age <= 20){
 //            ele.voted = false
@@ -204,7 +265,11 @@
 //     }
 // })
 // console.log(result)
-//************************************************************************************ */
+
+
+//*************************11**whwn type someones name show details as array********************************************************* */
+
+
 // var voters = [
 //     {name:'Bob' , age: 30, voted: true},
 //     {name:'Jake' , age: 32, voted: true},
@@ -219,6 +284,8 @@
 //     {name: 'Jeff', age: 30, voted: true},
 //     {name: 'Zack', age: 19, voted: false}
 // ];
+
+
 // const data = prompt("Enter a name:")
 // const details = voters.find((ele)=>{
 //     // console.log(ele)
@@ -231,7 +298,11 @@
 // }else{
 //     console.log("No data found")
 // }
+
+
 // ************************************************************************************************
+
+
 // var voters = [
 //     {name:'Bob' , age: 30, voted: true},
 //     {name:'Jake' , age: 32, voted: true},
@@ -246,6 +317,8 @@
 //     {name: 'Jeff', age: 30, voted: true},
 //     {name: 'Zack', age: 19, voted: false}
 // ];
+
+
 // const data = prompt("Enter a name:")
 // const details = voters.find((ele)=>{
 //     // console.log(ele)
@@ -253,11 +326,18 @@
 //         return ele
 //     }
 // })
+
+
 // if(details){
 //     console.log(details)
+
+
 // }else{
 //     console.log("No data found")
 // }
+
+
+// **********************************************************************************************************************
 
 
 // let newArr =[]
@@ -267,4 +347,66 @@
 // }
 // console.log(newArr)
 
- 
+
+// ************************************************************************************************************* 
+// git pull 
+// git pull origin master
+// ***************************************************************************************************
+
+// if any of number is 100 return true
+
+// const check =(a,b)=> a === 100 || b ===100
+// console.log(check(100,0))
+
+
+
+// ***********************function with argument with out return value
+
+// Function with arg without return value
+// function greet(name) {
+//     console.log("Hello, "k + name + "!");
+// }
+// greet("Alice"); 
+ // Output: Hello, Alice!
+// ////////////////////////////////////////////////////
+
+
+// Function with argument and with return value
+// function double(number) {
+//     return number * 2;
+// }
+// let result = double(5);
+// console.log(result);  // Output: 10
+//Function with argument and with return value 
+
+
+
+// **************************************factorial of a number*****************************************************
+
+
+// let x = prompt("Enter a Number:")
+//   num = 1;
+//  function factorial(x){
+//     for(let i= 1; i <= x; i++){
+//         num = num * i
+//     }
+//     return num
+// }
+// console.log("Factorial of number is" +" " + factorial(x))
+
+
+// *************************************************************************************************************
+
+
+// const x = prompt("Enter a Digit :")
+// let number = 1;
+// function factorial(x){
+    
+//     for(let i = 1; i <= x; i++){
+//         number = number * i
+//     }
+//     console.log("The factorial of numbeer is " + number)
+// }
+// factorial(x)
+
+// ***************************************************************************************************************
---
+++ notes/not.js
@@ -0,0 +1,127 @@
+
+// *******************function with arg with return
+// function double(number) {
+//     return number * 2;
+// }
+// let result = double(5);
+// console.log(result);  
+
+
+
+
+
+// *******************function with arg  without return
+// function greet(name) {
+//     console.log("Hello, " + name + "!");
+// }
+// greet("Alice");
+
+
+
+
+// *******************function without arg with return
+
+// function getRandomNumber() {
+//     return Math.random();
+// }
+// let randomNumber = getRandomNumber();
+// console.log(randomNumber);
+
+
+
+
+//  *******************function with0ut arg witoutreturn
+
+// function greet(){
+//     alert("hello")
+//     }
+//     greet();
+
+// ************************************************************************
+
+
+
+
+
+
+
+
+
+
+// swap value of 2 variables
+
+// let a = 1;
+// let b = 2;
+// [a,b] = [b,a];
+// console.log(a)
+// console.log(b)
+
+// ************************************************
+
+// swap value of 2 elements in an array
+
+// const colors = ["red","green","blue","black"];
+// [colors[0], colors[3]] = [colors[3],colors[0]];
+// console.log(colors)
+
+// **********************************************************
+
+// assign array elements to variable
+
+// const colors =["red","green","blue","black"];
+// const [firstColor, secondColor, thirdColor, ...extraColor] = colors
+// console.log(firstColor)
+// console.log(secondColor)
+// console.log(thirdColor)
+// console.log(extraColor)
+
+
+
+
+// **************************************************************
+
+// // Extract values from object
+
+// const person1 = {
+//     firstName: "Nick",
+//     lastName: "John",
+//     age: 34,
+// }
+// const person2 ={
+//     firstName: "Doe",
+//     lastName: "James",
+//     age: 24,
+// }
+// const{firstName, lastName, age} = person2;
+// console.log(firstName);
+// console.log(lastName);
+
+
+
+// *********************************************************
+
+// destructuring in function parameter
+
+// function displayPerson({firstName, lastName, age, job}){
+//     console.log(`name: ${firstName} ${lastName}`);
+//     console.log(`age: ${age}`);
+//     console.log(`job: ${job}`);
+// }
+// const person1 = {
+//     firstName: "Tom",
+//     lastName: "Doe",
+//     age: 30,
+//     job: "cook",
+// }
+// const person2 = {
+//     firstName: "Jo",
+//     lastName: "vicky",
+//     age: 20,
+// }
+// displayPerson(person2)
+
+
+// *************************************************************************
+
+
+
